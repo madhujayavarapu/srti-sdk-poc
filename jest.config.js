260c480854355df19cad30cfbbc72f13ab083400
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-10-10 16:47:31 +0530

    chore: add jest-html-reporter plugin for coverage report

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ export default {
       'ts-jest',
       {
         // ts-jest configuration goes here
-        tsconfig: './tsconfig.test.json'
+        tsconfig: './tsconfig.test.json',
       },
     ],
   },
@@ -15,5 +15,16 @@ export default {
     '\\.(scss|css)$': '<rootDir>/__mocks__/styleMock.js',
   },
   collectCoverage: true,
-  coverageReporters: ['json', 'lcov', 'text', 'clover']
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        pageTitle: 'SDK Test Report',
+        outputPath: '<rootDir>/coverage/test-reports/sdk-report.html',
+      },
+    ],
+  ],
+  coverageDirectory: '<rootDir>/coverage',
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import terser from '@rollup/plugin-terser'
-import scss from 'rollup-plugin-scss'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import scss from 'rollup-plugin-scss';
 import typescript from '@rollup/plugin-typescript';
 
-const version = '1.0.0'
+const version = '1.0.0';
 
 // ENV
-const outDir = 'dist'
-const filename = 'index'
-const isProduction = process.env.NODE_ENV === 'prod'
-console.log('Builds are generating for env:', process.env.NODE_ENV)
+const outDir = 'dist';
+const filename = 'index';
+const isProduction = process.env.NODE_ENV === 'prod';
+console.log('Builds are generating for env:', process.env.NODE_ENV);
 
 // common output options
 const outputOpts = {
   sourcemap: true,
   // TODO: Read the license file and add it as a banner.
-  banner: `/**@licenseSecuriti Inc. All rights reserved.\nLicense related info can be included here.\nVersion: ${version}*/`
-}
+  banner: `/**@licenseSecuriti Inc. All rights reserved.\nLicense related info can be included here.\nVersion: ${version}*/`,
+};
 
 export default {
   input: 'src/index.ts',
@@ -25,43 +25,44 @@ export default {
     {
       ...outputOpts,
       file: `${outDir}/${filename}.cjs.js`,
-      format: 'cjs'
+      format: 'cjs',
     },
     {
       ...outputOpts,
       file: `${outDir}/${filename}.esm.js`,
-      format: 'es'
+      format: 'es',
     },
     {
       ...outputOpts,
       name: 'cookieConsent',
       file: `${outDir}/${filename}.umd.js`,
-      format: 'umd'
-    }
+      format: 'umd',
+    },
   ],
   plugins: [
     scss({
       fileName: 'style.min.css',
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
     }),
     typescript({
-      tsconfig: "./tsconfig.json"
+      tsconfig: './tsconfig.json',
     }),
     resolve(),
     commonjs(),
-    isProduction && terser({
-      // to support IE8 browser. by default it is false.
-      ie8: true,
-      format: {
-        comments: function (node, comment) {
-          var text = comment.value;
-          var type = comment.type;
-          if (type == "comment2") {
-            // multiline comment
-            return /@preserve|@license/i.test(text);
-          }
-        }
-      },
-    })
-  ]
-}
+    isProduction &&
+      terser({
+        // to support IE8 browser. by default it is false.
+        ie8: true,
+        format: {
+          comments: function (node, comment) {
+            var text = comment.value;
+            var type = comment.type;
+            if (type == 'comment2') {
+              // multiline comment
+              return /@preserve|@license/i.test(text);
+            }
+          },
+        },
+      }),
+  ],
+};
