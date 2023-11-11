c8d09c1f68214229dc7b1527b86e5c69cc217e5f
Madhu Jayavarapu <madhujayavarapu523@gmail.com>
2023-11-11 16:36:28 +0530

    base url attribute changed to backend-url

--- src/constants/script.ts
+++ src/constants/script.ts
@@ -3,7 +3,7 @@ export const SCRIPT_TAG_SELECTOR = 'script[data-domain-uuid]';
 export const ATTRIBUTES = {
   TENANT_UUID: 'data-tenant-uuid',
   DOMAIN_UUID: 'data-domain-uuid',
-  BASE_URL: 'data-base-url',
+  BASE_URL: 'data-backend-url',
   CDN_BASE_URL: 'data-cdn-url',
   SKIP_CSS: 'data-skip-css',
   STAGING_MODE: 'data-securiti-staging-mode',
@@ -14,6 +14,5 @@ export const ATTRIBUTES = {
 export const REQUIRED_ATTRIBUTES = [
   ATTRIBUTES.TENANT_UUID,
   ATTRIBUTES.DOMAIN_UUID,
-  ATTRIBUTES.BASE_URL,
-  ATTRIBUTES.CDN_BASE_URL,
+  ATTRIBUTES.BASE_URL
 ];
